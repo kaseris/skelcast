fbdfb552423879a015cd1d3737f7cacd96f7d021
kaseris <mich.kaseris@gmail.com>
2023-11-07 14:16:02 +0200

    Method that creates a blacklist of files that have zero values

--- src/skelcast/data/prepare_data.py
+++ src/skelcast/data/prepare_data.py
@@ -33,3 +33,28 @@ def filter_missing(skeleton_files: list, missing_skeleton_names: list):
     ]
     print(f"Skeleton files after filtering: {len(filtered_skeleton_files)} files left.")
     return filtered_skeleton_files
+
+
+def should_blacklist(file_path: str):
+    with open(file_path, "r") as file:
+        try:
+            # Read the first line to get the number of following lines
+            num_lines = int(file.readline().strip())
+
+            # Read the next 'num_lines' lines
+            lines = [file.readline().strip() for _ in range(num_lines)]
+
+            # Check if all the lines are '0' and the count matches num_lines
+            if all(line == "0" for line in lines) and len(lines) == num_lines:
+                return True
+            else:
+                return False
+
+        except ValueError:
+            # Handle the case where the first line is not a number
+            print(f"Error: The file {file_path} does not start with a number.")
+            return False
+        except Exception as e:
+            # Handle other possible exceptions such as file not found, etc.
+            print(f"An error occurred: {e}")
+            return False

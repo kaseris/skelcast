ce253cf476e50aa9130942b44ac3f1d4d94f6753
kaseris <mich.kaseris@gmail.com>
2023-11-10 11:50:49 +0200

    Overfit batch experiment. Setup models and experiment runner

---
+++ src/skelcast/experiments/runner.py
@@ -0,0 +1,16 @@
+class Runner:
+    def __init__(self) -> None:
+        pass
+
+    def setup(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def training_step(self):
+        pass
+
+    def validation_step(self):
+        pass
+    

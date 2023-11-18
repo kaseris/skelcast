bb48a2f38d0685f8e24bf57a91b8ec21e1822e81
kaseris <mich.kaseris@gmail.com>
2023-11-18 20:19:15 +0200

    Model ready to train.

--- src/skelcast/models/rnn/lstm.py
+++ src/skelcast/models/rnn/lstm.py
@@ -3,17 +3,36 @@ import torch.nn as nn
 
 
 class SimpleLSTMRegressor(nn.Module):
-    def __init__(self, input_size,
+    def __init__(self,
                  hidden_size,
                  num_layers,
-                 batch_first: bool = True) -> None:
+                 batch_first: bool = True,
+                 num_bodies: int = 1,
+                 n_joints: int = 25,
+                 n_dims: int = 3) -> None:
         super().__init__()
+        self.num_bodies = num_bodies
+        self.n_joints = n_joints
+        self.n_dims = n_dims
+        input_size = self.num_bodies * self.n_joints * self.n_dims
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
                             batch_first=batch_first)
         self.linear = nn.Linear(in_features=hidden_size, out_features=input_size)
+        self.criterion = nn.MSELoss(reduction='mean')
     
-    def forward(self, x):
-        out = self.lstm(x)
-        out = self.linear(x)
+    def forward(self, x: torch.Tensor,
+                y: torch.Tensor = None):
+        assert x.ndim == 5, f'`x` must be a 5-dimensional tensor. Found {x.ndim} dimension(s).'
+        batch_size, context_size, num_bodies, n_joints, n_dims = x.shape
+        assert num_bodies == self.num_bodies, f'The number of bodies in the position 2 of the tensor is {num_bodies}, but it should be {self.num_bodies}'
+        assert n_joints == self.n_joints, f'The number of bodies in the position 3 of the tensor is {n_joints}, but it should be {self.n_joints}'
+        assert n_dims == self.n_dims, f'The number of bodies in the position 3 of the tensor is {n_dims}, but it should be {self.n_dims}'
+
+        x = x.view(batch_size, context_size, num_bodies * n_joints * n_dims)
+        out, _ = self.lstm(x)
+        out = self.linear(out)
+        if y is not None:
+            y = y.view(batch_size, context_size, num_bodies * n_joints * n_dims)
+            loss = self.criterion(out, y)
+            return out, loss
         return out
-    

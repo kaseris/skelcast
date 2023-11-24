356f6ddbf3aef6243f39233b2ce77b35ec0605fe
Michail Kaseris <mich.kaseris@gmail.com>
2023-11-24 16:13:41 +0200

    Transformers

---
+++ src/skelcast/models/transformers/transformer.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+import skelcast.models.transformers.base as base
+from skelcast.models import SkelcastModule
+
+class ForecastTransformer(SkelcastModule):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def training_step(self, *args, **kwargs):
+        return super().training_step(*args, **kwargs)
+    
+    def validation_step(self, *args, **kwargs):
+        return super().validation_step(*args, **kwargs)
+    
+    def predict(self, *args, **kwargs):
+        return super().predict(*args, **kwargs)

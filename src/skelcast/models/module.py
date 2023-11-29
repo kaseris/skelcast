17bc7baa07e0f403a79f4776dc860e4597363d76
Michail Kaseris <mich.kaseris@gmail.com>
2023-11-29 11:19:29 +0200

    Gradient flow storing for training debugging and analysis.

--- src/skelcast/models/module.py
+++ src/skelcast/models/module.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class SkelcastModule(nn.Module, metaclass=abc.ABCMeta):
     def __init__(self) -> None:
         super(SkelcastModule, self).__init__()
+        self.gradients = dict()
 
     @abc.abstractmethod
     def predict(self, *args, **kwargs):
@@ -26,3 +27,11 @@ class SkelcastModule(nn.Module, metaclass=abc.ABCMeta):
         Implements a validation step of a module
         """
         pass
+
+    def gradient_flow(self, *args, **kwargs):
+        """
+        Implements the gradient flow step of a module
+        """
+        for name, param in self.named_parameters():
+            if param.requires_grad and param.grad is not None:
+                self.gradients[name] = param.grad.clone()

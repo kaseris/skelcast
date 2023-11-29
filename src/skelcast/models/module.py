ad44147284527bb4be815d7d46529c1c5c2d1d70
Michail Kaseris <mich.kaseris@gmail.com>
2023-11-29 12:00:36 +0200

    Complete gradient information

--- src/skelcast/models/module.py
+++ src/skelcast/models/module.py
@@ -6,6 +6,7 @@ class SkelcastModule(nn.Module, metaclass=abc.ABCMeta):
     def __init__(self) -> None:
         super(SkelcastModule, self).__init__()
         self.gradients = dict()
+        self.gradient_update_ratios = dict()
 
     @abc.abstractmethod
     def predict(self, *args, **kwargs):
@@ -28,10 +29,30 @@ class SkelcastModule(nn.Module, metaclass=abc.ABCMeta):
         """
         pass
 
-    def gradient_flow(self, *args, **kwargs):
+    def gradient_flow(self):
         """
         Implements the gradient flow step of a module
         """
         for name, param in self.named_parameters():
             if param.requires_grad and param.grad is not None:
-                self.gradients[name] = param.grad.clone()
+                self.gradients[name] = param.grad.clone().detach().cpu().numpy()
+
+    def compute_gradient_update_norm(self, lr: float):
+        """
+        Computes the ratio of the parameter update to the parameter norm and stores it to the gradient_update_ratios
+        dictionary. The gradient update is approximated as the vanilla gradient descent update.
+
+        Args:
+        -    lr (float): The optimizer's learning rate
+        """
+        for name, param in self.named_parameters():
+            if param.requires_grad and param.grad is not None:
+                self.gradient_update_ratios[name] = (lr * param.grad.norm() / param.norm()).detach().cpu().numpy()
+
+    def get_gradient_histograms(self):
+        """
+        Returns the flat gradients of the module's parameters from the gradients dictionary.
+        """
+        return {name: param.grad.clone().view(-1).detach().cpu().numpy() for name, param in self.named_parameters() if
+                param.requires_grad and param.grad is not None}
+    

1c3ccd0758afa479d9df67286c0570f8c594f7fe
Michail Kaseris <mich.kaseris@gmail.com>
2023-12-07 12:01:03 +0200

    Dim fix for positional encoding

--- src/skelcast/models/transformers/base.py
+++ src/skelcast/models/transformers/base.py
@@ -65,11 +65,22 @@ class PositionalEncoding(nn.Module):
         # Create a long enough 'PE' matrix with position and dimension indexes
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+
+        # Adjust div_term calculation
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        
+
+        # Assign sine to even and cosine to odd indices
         pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
         
+        # Adjust cosine assignment for odd d_model
+        if d_model % 2 == 0:
+            pe[:, 1::2] = torch.cos(position * div_term)
+        else:
+            # For odd d_model, the last cosine term needs to be calculated differently
+            pe[:, 1::2] = torch.cos(position * div_term[:-1])
+            last_cos = torch.cos(position * div_term[-1])
+            pe[:, -1] = last_cos.squeeze()
+
         pe = pe.unsqueeze(0).transpose(0, 1)
         # Registers pe as a buffer that should not be considered a model parameter.
         self.register_buffer('pe', pe)

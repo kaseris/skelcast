4993bfb502245ee09a333ac32c8c3553f06edc49
Michail Kaseris <mich.kaseris@gmail.com>
2023-12-07 16:37:50 +0200

    Register encoders and decoders.

--- src/skelcast/models/__init__.py
+++ src/skelcast/models/__init__.py
@@ -2,7 +2,10 @@ from .module import SkelcastModule as SkelcastModule
 from skelcast.core.registry import Registry
 
 MODELS = Registry()
+ENCODERS = Registry()
+DECODERS = Registry()
 
 from .rnn.lstm import SimpleLSTMRegressor
 from .transformers.transformer import ForecastTransformer
 from .rnn.pvred import PositionalVelocityRecurrentEncoderDecoder
+from .rnn.pvred import Encoder, Decoder

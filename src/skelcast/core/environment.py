eae31046b05669323cd0c7f64b0f4a819d5d7d5c
Michail Kaseris <mich.kaseris@gmail.com>
2023-12-12 12:15:59 +0200

    Building transforms -> OK

--- src/skelcast/core/environment.py
+++ src/skelcast/core/environment.py
@@ -69,8 +69,8 @@ class Environment:
     def build_from_file(self, config_path: str) -> None:
         logging.log(logging.INFO, f'Building environment from {config_path}.')
         cfgs = read_config(config_path=config_path)
-        # TODO: Handle the case of transforms with a Compose object
         self._transforms = build_object_from_config(cfgs.transforms_config, TRANSFORMS)
+        print(self._transforms)
         # TODO: Add support for random splits
         self._dataset = build_object_from_config(cfgs.dataset_config, DATASETS)
         
@@ -83,3 +83,10 @@ class Environment:
         # If there's not a checkpoint directory, use the self._runner.fit() method
         # Otherwise, use the self._runner.resume(path_to_checkpoint) method
         return self._runner.fit()
+
+if __name__ == '__main__':
+    format = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=format)
+    env = Environment()
+    env.build_from_file('configs/pvred.yaml')
+    # env.run()

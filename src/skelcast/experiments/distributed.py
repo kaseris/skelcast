53b6b07c6f4b8bf18539897bc5eb071f30b63de9
Michail Kaseris <mich.kaseris@gmail.com>
2024-02-07 22:29:49 +0200

    Setup the imports

--- src/skelcast/experiments/distributed.py
+++ src/skelcast/experiments/distributed.py
@@ -1,3 +1,7 @@
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel
+from torch.distributed import init_process_group, destroy_process_group
+
 from skelcast.experiments import RUNNERS
 
 @RUNNERS.register_module()
